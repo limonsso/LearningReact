d7efa3575d80d8f41ffd08327c2901beb39300b9
limonsso <ezechiel.gnepa09@gmail.com>
2018-02-26 08:08:02 -0500

    les route intro

---
+++ 2-react-router/src/index.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Route} from 'react-router-dom';
+
+// COMPONENTS
+import Posts from './components/posts'
+import Profile from './components/profile'
+
+class  App extends Component  {
+  render() {
+    return <div>Home</div>
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/posts" component={Posts}></Route>
+      <Route path="/profile" component={Profile}></Route>
+  </div>
+</BrowserRouter>
+  ,document.querySelector('#root'))
